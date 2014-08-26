5672cb30083227b34bd150f11ca446e6f11549c6
lyubenhranov <lyuben.hranov@gmail.com>
2014-08-26 19:22:00 +0300

    Created the MusicFactory VS Solution

---
+++ MusicFactory/MusicFactory.Engine/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿namespace MusicFactory.Engine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
