7d5b1a005d35a9825268c9f3a512a432cfea785e
lyubenhranov <lyuben.hranov@gmail.com>
2014-09-03 11:40:23 +0300

    Pseudo code TO-DO in EntryPoint.cs

--- MusicFactory/MusicFactory.Client.Console/EntryPoint.cs
+++ MusicFactory/MusicFactory.Client.Console/EntryPoint.cs
@@ -10,11 +10,48 @@
     {
         static void Main()
         {
-            var order = new Order() { OrderDate = DateTime.Now.AddDays(-2), Price = 5 };
-            var order2 = new Order() { OrderDate = DateTime.Now.AddDays(2), Price = 5 };
-            var order3 = new Order() { OrderDate = DateTime.Now.AddDays(-2), Price = 2 };
-            var order4 = new Order() { OrderDate = DateTime.Now.AddDays(-2), Price = 1 };
-            var orders = new List<Order> { order, order2, order3, order4 };
+            // SASHO   
+            // Fill in MongoDB database with data
+
+            // Insert the data to SQL Server
+
+            // Read the data from Excel and transfer it to the SQL Server
+
+            // LYUBO
+            // Generate PDF and XML reports from SQL Server
+
+
+            // IVCHO
+            // Generate reports from SQL Server
+
+            // Create the MySQL database
+
+            // Put the reports in JSON and MySQL
+
+            //IVETO
+            // Read XML data
+
+            // Transfer the data to SQL Server and MongoDB
+
+            // SISI
+            // Get the reports from MySQL
+
+            // Get the additional data from SQLite
+
+            // Save the report to Excel 2007
+            //var musicFactoryContext = new MusicFactoryDbContext();
+
+            // LYUBO
+            // Console interface for reports
+
+            //var order = new Order() { OrderDate = DateTime.Now.AddDays(-2), Price = 5 };
+            //var order2 = new Order() { OrderDate = DateTime.Now.AddDays(2), Price = 5 };
+            //var order3 = new Order() { OrderDate = DateTime.Now.AddDays(-2), Price = 2 };
+            //var order4 = new Order() { OrderDate = DateTime.Now.AddDays(-2), Price = 1 };
+            //var orders = new List<Order> { order, order2, order3, order4 };
+
+            //musicFactoryContext.Orders.Add(order);
+            //musicFactoryContext.SaveChanges();
            // var pers = new ExcelPersister();
 
 
